dad05ee69242f6998dbef4d6247950be470a8362
Filip Mihocas <filip.mihocas@gmail.com>
2024-10-26 18:10:18 +0300

    Refactor layout and styling in Footer and Header components

--- faithbase/src/components/Footer.jsx
+++ faithbase/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from '../assets/logo.svg';
 export default function Footer() {
 	return (
 		<footer className="pt-4">
-			<div className="mx-auto px-4 sm:px-6 lg:px-8 py-12">
+			<div className="mx-auto py-12">
 				<div className="grid grid-cols-1 md:grid-cols-4 gap-8">
 					<div className="space-y-4">
 						<div className="flex items-center space-x-2">
--- faithbase/src/components/Header.jsx
+++ faithbase/src/components/Header.jsx
@@ -1,32 +1,47 @@
-import { Button } from "@/components/ui/button";
-import Logo from "../assets/Logo.svg";
+import { Button } from '@/components/ui/button';
+import Logo from '../assets/Logo.svg';
 
 export default function Header() {
-  return (
-    <header className="py-4 pt-8 flex items-center justify-between pb-10">
-      <div className="flex items-center space-x-2">
-        <img src={Logo} alt="FaithbaseLogo" />
-      </div>
-      <nav className="flex items-center space-x-6">
-        <Button
-          variant="default"
-          className="bg-blue-500 hover:bg-blue-600 text-white"
-        >
-          Assistant
-        </Button>
-        <a href="/find-doctor" className="text-gray-600 hover:text-gray-800">
-          Find a doctor
-        </a>
-        <a href="/apps" className="text-gray-600 hover:text-gray-800">
-          Apps
-        </a>
-        <a href="/testimonials" className="text-gray-600 hover:text-gray-800">
-          Testimonials
-        </a>
-        <a href="/about" className="text-gray-600 hover:text-gray-800">
-          About us
-        </a>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="py-4 pt-8 flex items-center justify-between pb-10">
+			<div className="flex items-center space-x-2">
+				<img
+					src={Logo}
+					alt="FaithbaseLogo"
+				/>
+			</div>
+			<nav className="flex items-center space-x-6">
+				<Button
+					variant="default"
+					className="bg-blue-500 hover:bg-blue-600 text-white"
+				>
+					<a href="/pacient">Pacient</a>
+				</Button>
+				<a
+					href="/doctor"
+					className="text-gray-600 hover:text-gray-800"
+				>
+					Doctor
+				</a>
+				<a
+					href="/apps"
+					className="text-gray-600 hover:text-gray-800"
+				>
+					Settings
+				</a>
+				<a
+					href="/testimonials"
+					className="text-gray-600 hover:text-gray-800"
+				>
+					Testimonials
+				</a>
+				<a
+					href="/about"
+					className="text-gray-600 hover:text-gray-800"
+				>
+					About us
+				</a>
+			</nav>
+		</header>
+	);
 }
