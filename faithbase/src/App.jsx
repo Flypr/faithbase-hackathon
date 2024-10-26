2e63c41606b68bc40263dc3703f8361f5acfd25b
Daniel Buret <danila.buret@creatopy.com>
2024-10-26 15:23:46 +0300

    Footer and MedicalDashboard

--- faithbase/src/App.jsx
+++ faithbase/src/App.jsx
@@ -1,17 +1,21 @@
-import Header from './components/Header';
-import UserInfo from './components/UserInfo';
-import DoctorRecomendationsCard from './components/DoctorRecomendationsCard';
+import Header from "./components/Header";
+import UserInfo from "./components/UserInfo";
+import DoctorRecomendationsCard from "./components/DoctorRecomendationsCard";
+import Footer from "./components/Footer";
+import MedicalDashboard from "./components/MedicalDashboard";
 
 function App() {
-	return (
-		<main className="px-12 h-screen">
-			<Header />
-			<div className="flex gap-6">
-				<UserInfo />
-				<DoctorRecomendationsCard />
-			</div>
-		</main>
-	);
+  return (
+    <main className="px-12">
+      <Header />
+      <div className="flex gap-6">
+        <UserInfo />
+        <DoctorRecomendationsCard />
+      </div>
+      <MedicalDashboard />
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
