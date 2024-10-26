7e57caabaacd7855e30dbf05cccaf86d8c722594
Daniel Buret <danila.buret@creatopy.com>
2024-10-26 21:17:11 +0300

    Refactor DoctorApp and PacientApp components to include UserInfo and DoctorRecommendationsCard

--- faithbase/src/PacientApp.jsx
+++ faithbase/src/PacientApp.jsx
@@ -1,21 +1,21 @@
-import Header from './components/Header';
-import UserInfo from './components/UserInfo';
-import DoctorRecomendationsCard from './components/DoctorRecomendationsCard';
-import Footer from './components/Footer';
-import MedicalDashboard from './components/MedicalDashboard';
+import Header from "./components/Header";
+import UserInfo from "./components/UserInfo";
+import Footer from "./components/Footer";
+import MedicalDashboard from "./components/MedicalDashboard";
+import DoctorRecommendationsCard from "./components/DoctorRecomendationsCard";
 
 function PacientApp() {
-	return (
-		<main className="px-12">
-			<Header />
-			<div className="grid grid-cols-12 gap-4">
-				<UserInfo />
-				<DoctorRecomendationsCard />
-			</div>
-			<MedicalDashboard />
-			<Footer />
-		</main>
-	);
+  return (
+    <main className="px-12">
+      <Header />
+      <div className="grid grid-cols-12 gap-4">
+        <UserInfo />
+        <DoctorRecommendationsCard role="Pacient" />
+      </div>
+      <MedicalDashboard />
+      <Footer />
+    </main>
+  );
 }
 
 export default PacientApp;
