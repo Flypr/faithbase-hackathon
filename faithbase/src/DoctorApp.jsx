fd4b122c277b920ef01a894f1c6ed8ca0d21dd3d
Daniel Buret <danila.buret@creatopy.com>
2024-10-26 16:50:45 +0300

    add routes

---
+++ faithbase/src/DoctorApp.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main className="px-12">
+      <Header />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
