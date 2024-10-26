345e56dcbe9c467ab37e7a09df424b5264a8d1df
Daniel Buret <danila.buret@creatopy.com>
2024-10-26 14:57:02 +0300

    Added header

--- faithbase/src/components/Header.jsx
+++ faithbase/src/components/Header.jsx
@@ -1,5 +1,32 @@
-const Header = () => {
-  return <div className="text-3xl font-bold underline">TEST COMMIT</div>;
-};
+import { Button } from "@/components/ui/button";
+import Logo from "../assets/Logo.svg";
 
-export default Header;
+export default function Header() {
+  return (
+    <header className="py-4 px-6 flex items-center justify-between">
+      <div className="flex items-center space-x-2">
+        <img src={Logo} alt="FaithbaseLogo" />
+      </div>
+      <nav className="flex items-center space-x-6">
+        <Button
+          variant="default"
+          className="bg-blue-500 hover:bg-blue-600 text-white"
+        >
+          Assistant
+        </Button>
+        <a href="/find-doctor" className="text-gray-600 hover:text-gray-800">
+          Find a doctor
+        </a>
+        <a href="/apps" className="text-gray-600 hover:text-gray-800">
+          Apps
+        </a>
+        <a href="/testimonials" className="text-gray-600 hover:text-gray-800">
+          Testimonials
+        </a>
+        <a href="/about" className="text-gray-600 hover:text-gray-800">
+          About us
+        </a>
+      </nav>
+    </header>
+  );
+}
