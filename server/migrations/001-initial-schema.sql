dd53e32ec88ae2cffa746ae1adfb2cdc68c8360a
Anonymus1145 <Anonymus1145@yahoo.com>
2024-10-27 10:14:47 +0200

    Last hope

--- server/migrations/001-initial-schema.sql
+++ server/migrations/001-initial-schema.sql
@@ -1,74 +1,74 @@
 -- Up
 
-CREATE TABLE Patients (
-    Patient_id INTEGER PRIMARY KEY,
-    First_name TEXT NOT NULL,
-    Personal_numeric_number INTEGER NOT NULL,
-    Last_name TEXT NOT NULL,
-    Date_of_birth TEXT NOT NULL,
-    Gender TEXT,
-    Age INTEGER,
-    Blood_group TEXT,
-    Phone TEXT,
-    Email TEXT,
-    Symptoms_description TEXT,
-    Patient_fo_number INTEGER,
-    Last_ai_model_response TEXT NOT NULL,
-    Address TEXT NOT NULL
+CREATE TABLE patients (
+    patient_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    personal_numeric_number INTEGER NOT NULL,
+    last_name TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,
+    gender TEXT,
+    age INTEGER,
+    blood_group TEXT,
+    phone TEXT,
+    email TEXT,
+    symptoms_description TEXT,
+    patient_fo_number INTEGER,
+    last_ai_model_response TEXT NOT NULL,
+    address TEXT NOT NULL
 );
 
 
-CREATE TABLE Medical_history (
-    Medical_history_id INTEGER PRIMARY KEY,
-    Patient_id INTEGER NOT NULL,
-    Diagnosis TEXT NOT NULL,
-    Examination TEXT NOT NULL,
-    Examination_date TEXT NOT NULL,
-    FOREIGN KEY (Patient_id) REFERENCES Patients (Patient_id)
+CREATE TABLE medical_history (
+    medical_history_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    diagnosis TEXT NOT NULL,
+    examination TEXT NOT NULL,
+    examination_date TEXT NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients (patient_id)
 );
 
-CREATE TABLE Medications (
-    Medication_id INTEGER PRIMARY KEY,
-    Patient_id INTEGER NOT NULL,
-    Medication TEXT NOT NULL,
-    FOREIGN KEY (Patient_id) REFERENCES Patients (Patient_id)
+CREATE TABLE medications (
+    medication_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    medication TEXT NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients (patient_id)
 );
 
-CREATE TABLE Vital_signs (
-    Vital_sign_id INTEGER PRIMARY KEY,
-    Patient_id INTEGER NOT NULL,
-    Heart_rate TEXT NOT NULL,
-    Blood_pressure TEXT NOT NULL,
-    Temperature INTEGER NOT NULL,
-    Blood_sugar INTEGER NOT NULL,
-    FOREIGN KEY (Patient_id) REFERENCES Patients (Patient_id)
+CREATE TABLE vital_signs (
+    vital_sign_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    heart_rate TEXT NOT NULL,
+    blood_pressure TEXT NOT NULL,
+    temperature INTEGER NOT NULL,
+    blood_sugar INTEGER NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients (patient_id)
 );
 
-CREATE TABLE Lab_results (
-    Lab_result_id INTEGER PRIMARY KEY,
-    Patient_id INTEGER NOT NULL,
-    Date_of_test TEXT NOT NULL,
-    Test_name TEXT NOT NULL,
-    Test_result TEXT NOT NULL,
-    Lab_recomandation TEXT NOT NULL,
-    FOREIGN KEY (Patient_id) REFERENCES Patients (Patient_id)
+CREATE TABLE lab_results (
+    lab_result_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    date_of_test TEXT NOT NULL,
+    test_name TEXT NOT NULL,
+    test_result TEXT NOT NULL,
+    lab_recomandation TEXT NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients (patient_id)
 );
 
-CREATE TABLE Treatment_plans (
-    Treatment_plan_id INTEGER PRIMARY KEY,
-    Patient_id INTEGER NOT NULL,
-    Date_of_treatment TEXT NOT NULL,
-    Doctor_name TEXT NOT NULL,
-    Treatment TEXT NOT NULL,
-    FOREIGN KEY (Patient_id) REFERENCES Patients (Patient_id)
+CREATE TABLE treatment_plans (
+    treatment_plan_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    date_of_treatment TEXT NOT NULL,
+    doctor_name TEXT NOT NULL,
+    treatment TEXT NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients (patient_id)
 );
 
 -- Down
 
-DROP TABLE Patients;
-DROP TABLE Medical_history;
-DROP TABLE Medications;
-DROP TABLE Vital_signs;
-DROP TABLE Lab_results;
-DROP TABLE Treatment_plans;
-DROP TABLE Notes;
+DROP TABLE patients;
+DROP TABLE medical_history;
+DROP TABLE medications;
+DROP TABLE vital_signs;
+DROP TABLE lab_results;
+DROP TABLE treatment_plans;
+DROP TABLE notes;
